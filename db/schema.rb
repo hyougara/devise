89940c78e5a7f856cba77dddd4e5e2a350c92bc7
前田優樹 <orzazous2@gmail.com>
2018-11-21 23:51:51 +0900

    一応管理userは作れた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_164261) do
+ActiveRecord::Schema.define(version: 2018_11_21_135036) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2018_11_20_164261) do
     t.string "uid"
     t.string "username"
     t.string "image_name"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
